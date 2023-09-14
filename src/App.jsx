4ddc95131f7ea0bfa1ff8b519bcf593e8a816748
maruf02 <rashiduzzaman99@gmail.com>
2023-09-15 03:00:54 +0600

    Note: fetch json data and show output console log

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Header from './components/header/Header'
 
+import Cards from './components/Cards/Cards'
+
 function App() {
   
 
@@ -10,7 +12,8 @@ function App() {
       <div className='bg-white h-[3000px]'>
         <div className='container mx-auto'>
           <Header></Header>
-        <hr className='w-11/12 mx-auto my-5' />
+          <hr className='w-11/12 mx-auto my-5' />
+          <Cards></Cards>
         </div>
       </div>
       
